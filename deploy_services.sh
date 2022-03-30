f320c020db86d341dfa5f0f23ef6ed8417d52a7a
lachlan-ng <lng@frontiersi.com.au>
2022-03-30 16:27:58 +1100

    Initial commit

---
+++ deploy_services.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# Deploy systemd services on the 5G Testbed RaspberryPi.
+
+# Get variables from config file
+source config.cfg
+
+# Get this scripts absolute path
+SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+
+# Copy systemd services
+echo "Copying systemd services to /lib/systemd/system/..."
+sudo cp $SCRIPT_DIR/services/5gtb-daemon.service /lib/systemd/system/
+sudo cp $SCRIPT_DIR/services/wait-for-network.service /lib/systemd/system/
+
+# Write username into the absolute path of the startup script in the service
+echo "Writing username into services..."
+sudo sed -i "s/<USER>/$USER/g" /lib/systemd/system/5gtb-daemon.service
+
+# Restart systemd-daemon
+echo "Reloading systemd daemon..."
+sudo systemctl daemon-reload
+
+# Enable services
+echo "Enabling services..."
+sudo systemctl enable wait-for-network.service
+sudo systemctl enable 5gtb-daemon.service
+
+# Start services
+echo "Starting services..."
+sudo systemctl start wait-for-network.service
+sudo systemctl start 5gtb-daemon.service
+
+echo "All done!"
---
+++ startup/5gtb_startup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# Execute the SUPL LPP client, stream NMEA and RTCM to serial port and log to file.
+
+# Get this scripts absolute path
+SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+
+# Make the data output directory if it doesn't exist
+mkdir -p $HOME/output
+
+# Get variables from config file
+source ${SCRIPT_DIR}/../config.cfg
+
+# Check that mode is correctly formatted
+if !([ ${mode} = "positioning" ] || [ ${mode} = "correction" ]) ; then
+    echo "Invalid mode"
+fi
+
+# Positioning mode
+if [ ${mode} = "positioning" ]; then
+    echo "Starting positioning mode"
+    
+    # Configure serial port
+    echo "Configuring ${serial_port}"
+    stty -F ${serial_port} ${baud_rate} -echo
+
+    # Save NMEA messages from serial port to file
+    echo "Logging ${serial_port} to $HOME/output/`date +"%Y%m%d-%H%M%S"`.nmea"
+    cat "${serial_port}" > $HOME/output/`date +"%Y%m%d-%H%M%S"`.nmea &
+
+    # Execute supl client, get corrections from location server and stream RTCM to serial port
+    echo "Executing SUPL LPP Client"
+    exec supl-lpp-client -h ${host} -p ${port} -c ${mcc} -n ${mnc} -t ${tac} -i ${cell_id} -d ${serial_port} -r ${baud_rate} -x $HOME/output/`date +"%Y%m%d-%H%M%S"`.rtcm
+fi
+
+# Correction mode
+if [ ${mode} = "correction" ]; then   
+    echo "Starting correction mode"
+
+    # Execute supl client, save RTCM to file and stream RTCM to serial port
+    echo "Executing SUPL LPP Client"
+    exec supl-lpp-client -h ${host} -p ${port} -c ${mcc} -n ${mnc} -t ${tac} -i ${cell_id} -d ${serial_port} -x $HOME/output/`date +"%Y%m%d-%H%M%S"`.rtcm &
+fi
