2c4a0af938f0d06db87a5c500d6e24e8a11d672d
lachlan-ng <lng@frontiersi.com.au>
2022-04-07 18:54:49 +1000

    Use GPSD to allow multiple streams from serial port - log to file and stream to TCP server

--- startup/5gtb_startup.sh
+++ startup/5gtb_startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Execute the SUPL LPP client, stream NMEA and RTCM to serial port and log to file.
+# Execute the SUPL client, stream NMEA and RTCM to serial port and log to file.
 
 # Get this scripts absolute path
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
@@ -21,26 +21,56 @@ if [ ${mode} = "positioning" ]; then
     echo "Starting positioning mode"
     
     # Configure serial port
-    echo "Configuring ${serial_port}"
+    echo "Configuring serial port ${serial_port}"
     stty -F ${serial_port} ${baud_rate} -echo
 
-    # Save NMEA messages from serial port to file
-    echo "Logging ${serial_port} to $HOME/output/`date +"%Y%m%d-%H%M%S"`.nmea"
-    exec cat "${serial_port}" > $HOME/output/`date +"%Y%m%d-%H%M%S"`.nmea &
+    # Execute GPSD in foreground, read only and in listening mode
+    echo "Executing GPSD on ${serial_port}"
+    sudo gpsd -N -b -G ${serial_port} &
 
-    # Execute supl client, get corrections from location server and stream RTCM to serial port
+    # Execute SUPL client stream RTCM to serial port/file, log stdout to file
+    # This needs to be executed before gpspipe, as this initialises GPSD on the
+    # serial port, which will then hog the serial port
     echo "Executing SUPL LPP Client"
-    exec supl-lpp-client -h ${host} -p ${port} -c ${mcc} -n ${mnc} -t ${tac} -i ${cell_id} -d ${serial_port} -r ${baud_rate} -x $HOME/output/`date +"%Y%m%d-%H%M%S"`.rtcm
+    echo "Logging RTCM to" \
+        "${output_dir%/}/`date +"%Y%m%d-%H%M%S"`-$HOSTNAME.rtcm"
+    exec supl-lpp-client \
+        -h ${host} -p ${port} -c ${mcc} -n ${mnc} -t ${tac} -i ${cell_id} \
+        -d ${serial_port} -r ${baud_rate} \
+        -x ${output_dir%/}/`date +"%Y%m%d-%H%M%S"`-$HOSTNAME.rtcm > \
+        ${output_dir%/}/`date +"%Y%m%d-%H%M%S"`-$HOSTNAME.log &
+
+    # Sleep to allow for the SUPL client to initialise the serial port
+    sleep 0.5
+
+    # Save NMEA messages from serial port to file
+    echo "Logging NMEA to" \
+        "${output_dir%/}/`date +"%Y%m%d-%H%M%S"`-$HOSTNAME.nmea"
+    gpspipe -R > ${output_dir%/}/`date +"%Y%m%d-%H%M%S"`-$HOSTNAME.nmea &
+
+    # Pipe NMEA messages into TCP server port 6000
+    echo "Streaming NMEA to TCP server port 6000"
+    socat TCP-LISTEN:6000,fork,reuseaddr EXEC:'gpspipe -R' &
+    # Can add TCP servers here if required
 fi
 
 # Correction mode
-if [ ${mode} = "correction" ]; then   
+if [ ${mode} = "correction" ]; then
     echo "Starting correction mode"
 
     # Execute supl client, save RTCM to file and stream RTCM to serial port
     echo "Executing SUPL LPP Client"
-    exec supl-lpp-client -h ${host} -p ${port} -c ${mcc} -n ${mnc} -t ${tac} -i ${cell_id} -d ${serial_port} -x $HOME/output/`date +"%Y%m%d-%H%M%S"`.rtcm
+    echo "Streaming RTCM to ${serial_port}"
+    exec supl-lpp-client \
+        -h ${host} -p ${port} -c ${mcc} -n ${mnc} -t ${tac} -i ${cell_id} \
+        -d ${serial_port} -x ${output_dir%/}/`date +"%Y%m%d-%H%M%S"`.rtcm > \
+        ${output_dir%/}/`date +"%Y%m%d-%H%M%S"`-$HOSTNAME.log &
 fi
+
+# Keep script alive
+while :; do
+    sleep 5
+done
 
 # Kill background jobs on exit
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
