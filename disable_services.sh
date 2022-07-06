e872c77dec07e32765bc76754185f9b0c4857262
lachlan-ng <lng@frontiersi.com.au>
2022-07-06 11:18:11 +1000

    Stop before disable

--- disable_services.sh
+++ disable_services.sh
@@ -2,14 +2,14 @@
 #
 # Disable systemd services on the 5G Testbed RaspberryPi.
 
-# Disable services
-echo "Disabling services..."
-sudo systemctl disable wait-for-network.service
-sudo systemctl disable 5gtb-daemon.service
-
 # Stop services
 echo "Stopping services..."
 sudo systemctl stop wait-for-network.service
 sudo systemctl stop 5gtb-daemon.service
+
+# Disable services
+echo "Disabling services..."
+sudo systemctl disable wait-for-network.service
+sudo systemctl disable 5gtb-daemon.service
 
 echo "All done!"
