61cc248b0eef6bd1600f9e8e9db9fe7c3c932416
lachlan-ng <lng@frontiersi.com.au>
2022-07-27 13:03:27 +1000

    Support GMV PE v0.0.2 with integrated SUPL client

--- startup/5gtb_startup.sh
+++ startup/5gtb_startup.sh
@@ -2,22 +2,22 @@
 #
 # Execute the SUPL client, stream NMEA and RTCM to serial port and log to file.
 
-# Set timezone for date command
-export TZ=Australia/Sydney
-
 # Kill script if a command returns a non-zero status (error)
 set -e
 
-# Stop docker container and kill child processes on exit
-trap "docker ps -q --filter "name=PE_AUS_5GTB" | grep -q . && \
-      docker stop PE_AUS_5GTB && trap - SIGTERM && \
-      kill -- -$$" SIGINT SIGTERM EXIT
+# Set timezone for date command
+export TZ=Australia/Sydney
 
 # Get this scripts absolute path
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
 # Get variables from config file
 source ${SCRIPT_DIR}/../config.cfg
+
+# Stop docker container and kill child processes on exit
+trap "trap - SIGTERM && \
+      docker-compose -f ${gmv_pe_dir%/}/docker-compose.yml stop && \
+      kill -- -$$" SIGINT SIGTERM EXIT
 
 # Check that mode is correctly formatted
 if !([ ${mode} = "positioning" ] || [ ${mode} = "correction" ]) ; then
@@ -32,10 +32,17 @@ fi
 # Positioning mode
 if [ ${mode} = "positioning" ]; then
     echo "Starting Positioning Mode [${correction_type}]"
-    
-    # Configure serial port
     echo "Configuring serial port ${uart_serial_port}"
-    stty -F ${uart_serial_port} ${baud_rate} -echo
+
+    # If set, save SBF to file
+    if [ ! -z ${usb_serial_port} ]; then
+        echo "Executing str2str on ${usb_serial_port}"
+        echo "Logging SBF to" \
+            "${output_dir%/}/`date +"%Y%m%d-%H%M%S"`-$HOSTNAME.sbf"
+
+        str2str -in serial://${usb_serial_port##*/}:${baud_rate} \
+            -out file://${output_dir%/}/`date +"%Y%m%d-%H%M%S"`-$HOSTNAME.sbf &
+    fi
 
     # For OSR corrections direct RTCM corrections from the SUPL client to the GNSS HAT
     if [ ${correction_type} = "osr" ]; then
@@ -63,47 +70,15 @@ if [ ${mode} = "positioning" ]; then
             # Can add another output here if required
     fi
 
-    # For SSR corrections direct RTCM corrections from the SUPL client to a TCP server
+    # For SSR corrections execute the GMV Positioning Engine
     if [ ${correction_type} = "ssr" ]; then
-
-        # Deploy Docker container with GMV's Positioning Engine
-        # Within the Docker entrypoint, a RTKLIB TCP server is instantiated that
-        # takes the SUPL Client's RTCM as input and outputs them to the GMV PE
+        # Deploy Docker container with GMV's Positioning Engine and the SUPL LPP Client
         echo "Executing Positioning Engine"
-        docker run -itd --rm -v /home/pi/5gtb-pe/Docker_5GTB/:/opt/magic/RT_PE \
-            -v ${output_dir}:/opt/usb \
-            -p 19500:19500 -p 60001:60001 --device=/dev/ttyAMA0 \
-            --name PE_AUS_5GTB rtpe_aus:latest &
-
-        # Execute supl client, save RTCM to file and stream RTCM to TCP Server
-        echo "Executing SUPL LPP Client"
-        echo "Logging RTCM to" \
-            "${output_dir%/}/`date +"%Y%m%d-%H%M%S"`-$HOSTNAME.rtcm"
-        supl-lpp-client \
-            -h ${host} -p ${port} -c ${mcc} -n ${mnc} -t ${tac} -i ${cell_id} \
-            -k 127.0.0.1 -o 60001 \
-            -x ${output_dir%/}/`date +"%Y%m%d-%H%M%S"`-$HOSTNAME.rtcm |
-            # Prepend UTC timestamps to log file
-            while IFS= read -r line; do
-                printf '%s %s\n' "$(date '+%Y-%m-%d %H:%M:%S %Z')" "$line";
-            done > ${output_dir%/}/`date +"%Y%m%d-%H%M%S"`-$HOSTNAME.log &
-
-        # Log NMEA to file
         echo "Logging NMEA to" \
             "${output_dir%/}/`date +"%Y%m%d-%H%M%S"`-$HOSTNAME.nmea"
+        exec docker-compose -f ${gmv_pe_dir%/}/docker-compose.yml up -d &
         str2str -in tcpcli://127.0.0.1:19500 \
             -out file://${output_dir%/}/`date +"%Y%m%d-%H%M%S"`-$HOSTNAME.nmea
-    fi
-
-    # If set, save SBF to file
-    if [ ! -z ${usb_serial_port} ]; then
-        echo "Executing str2str on ${usb_serial_port}"
-        echo "Logging SBF to" \
-            "${output_dir%/}/`date +"%Y%m%d-%H%M%S"`-$HOSTNAME.sbf"
-        stty -F ${usb_serial_port} ${baud_rate} -echo
-
-        str2str -in serial://${usb_serial_port##*/}:${baud_rate} \
-            -out file://${output_dir%/}/`date +"%Y%m%d-%H%M%S"`-$HOSTNAME.sbf &
     fi
 fi
 
